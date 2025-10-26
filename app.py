12ce72f1cff802778077c22f8f14068c90345c40
Prisha <prisha@pblenovo.localdomain>
2025-10-26 15:09:06 -0400

    fixed another error

--- app.py
+++ app.py
@@ -82,5 +82,5 @@ def health():
 def index():
     return render_template("index.html")
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8000"))
+    port = int(os.getenv("PORT", "5000"))
     app.run(host="0.0.0.0", port=port, debug=True)
