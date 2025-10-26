91623e485237000586610565ce54d73665601931
Prisha <prisha@pblenovo.localdomain>
2025-10-26 14:57:19 -0400

    fixed another error

--- app.py
+++ app.py
@@ -81,6 +81,5 @@ def health():
 @app.get("/")
 def index():
     return render_template("index.html")
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=0, debug=True)
